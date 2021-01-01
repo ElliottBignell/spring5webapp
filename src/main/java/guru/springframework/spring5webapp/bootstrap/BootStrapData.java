fa185cb8de7b0f8584e8d0c1d803d9c8edbba991
Elliott Bignell <elliott.bignell@gmail.com>
2021-01-01 17:34:21 +0100

    Added templates but getting 404

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,6 +5,7 @@ import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +17,24 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(
+            AuthorRepository authorRepository,
+            BookRepository bookRepository,
+            PublisherRepository pubRepository
+    ) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = pubRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher pub = new Publisher( "Kackson-Pollock", "This Street", "Gotham", "New York", "12345" );
+
+        publisherRepository.save(pub);
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
@@ -41,10 +52,15 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        ddd.setPublisher(pub);
+        pub.getBooks().add(ddd);
+
+        noEJB.setPublisher(pub);
+        pub.getBooks().add(noEJB);
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
-
-        Publisher pub = new Publisher( "Kackson-Pollock", "This Street", "Gotham", "New York", "12345" );
         System.out.println(pub);
+        System.out.println("Number of books : " + pub.getBooks().size());
     }
 }
